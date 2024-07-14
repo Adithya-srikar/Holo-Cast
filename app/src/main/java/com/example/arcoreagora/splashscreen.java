b1c4176963d376cd7133d377ff72e03a0b240e34
babasriharsha <baba.sriharsha@gmail.com>
2024-07-14 14:37:23 +0530

    updated splash screen+firebase(signup activity)

--- app/src/main/java/com/example/arcoreagora/splashscreen.java
+++ app/src/main/java/com/example/arcoreagora/splashscreen.java
@@ -1,25 +1,45 @@
 package com.example.arcoreagora;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class splashscreen extends AppCompatActivity {
+    private static final int SPLASH_DISPLAY_LENGTH = 3000; // Duration in milliseconds (3 seconds)
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splashscreen);
-        Handler handler = new Handler();
-        handler.postDelayed(new Runnable() {
+
+        ImageView logoImageView = findViewById(R.id.logoImageView);
+        ProgressBar progressBar = findViewById(R.id.progressBar);
+
+
+        // Load animations
+        Animation fadeIn = AnimationUtils.loadAnimation(this, R.anim.fade_in);
+        Animation scaleUp = AnimationUtils.loadAnimation(this, R.anim.scale_up);
+
+        // Start animations
+        logoImageView.startAnimation(fadeIn);
+        logoImageView.startAnimation(scaleUp);
+        progressBar.startAnimation(fadeIn);
+
+
+        // Handler to start the main activity and close this splash screen after a delay
+        new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                startActivity(new Intent(splashscreen.this,LoginActivity.class));
-                finish();
+                Intent mainIntent = new Intent(splashscreen.this, LoginActivity.class);
+                splashscreen.this.startActivity(mainIntent);
+                splashscreen.this.finish();
             }
-        },4000);
+        }, SPLASH_DISPLAY_LENGTH);
     }
-
 }
